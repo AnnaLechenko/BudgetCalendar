d8f24036ae732487bb0c82778aa9609ed26e0950
Anna <anya.lechenko@mail.ru>
2025-03-19 20:43:55 +0300

    add navigation or menu

--- app/src/main/java/com/annalech/budgetcalendar/presentation/MainActivity.kt
+++ app/src/main/java/com/annalech/budgetcalendar/presentation/MainActivity.kt
@@ -2,12 +2,31 @@ package com.annalech.budgetcalendar.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.annalech.budgetcalendar.R
+import com.annalech.budgetcalendar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navigateController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navigateController = navHostFragment.findNavController()
+
+        binding.bottonNavBar.setupWithNavController(navigateController)
+
 
     }
 }
